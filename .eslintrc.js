3f9517bdeabb761ce5d576f43915b5d689ae4842
Akshit Grover <akshit.grover2016@gmail.com>
2018-09-11 16:46:19 +0530

    Add Lint Rules

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "rules": {
+        "semi":["error","always"],
+        "brace-style":["error","1tbs"],
+        "quotes":["error","double"],
+        "eol-last":["error","always"],
+        "block-spacing":["error","never"],
+        "comma-spacing":["error",{"before":false,"after":true}]
+    }
+};
