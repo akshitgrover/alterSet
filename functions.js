50c330df0260833e87bca6d25d7e889e00876c40
Akshit Grover <akshit.grover2016@gmail.com>
2018-09-14 17:36:29 +0530

    Add sort array func

--- functions.js
+++ functions.js
@@ -28,12 +28,53 @@ let fromJson = (json)=>{
 
 };
 
+/**
+ * Sort a given array (passed as argument)
+ * @param {Array} arr - Array to be sorted
+ * @param {String} [order=ASC] - Ascending `ASC` | Descending `DESC` 
+ * @memberof func
+ * @returns {Array} - Sorted Array 
+ */
+let sort = (arr, order = "ASC")=>{
+    
+    if(typeof order !== "string"){
+        let e = new Error(`order should be 'string' got ${typeof order}`);
+        e.code = "ERR_INVALID_ARG_TYPE";
+        throw e;
+    }
+    if(order !== "ASC" && order !== "DESC"){
+        let e = new Error(`value of order should be 'ASC' or 'DESC' got ${order}`);
+        e.code = "ERR_INVALID_ARG_VALUE";
+        throw e;
+    }
+
+    let sortedArr = [];
+    switch(order){
+
+        case "ASC":
+            sortedArr = arr.sort((p, n)=>{
+                return p - n;
+            });
+            break;
+        
+        case "DESC":
+            sortedArr = arr.sort((p, n)=>{
+                return n - p;
+            });
+            break;
+
+    }
+    return sortedArr;
+
+};
+
 //Export functions
 module.exports = (AlterSet)=>{
     
     return {
         addGlobal: addGlobal.bind(addGlobal, AlterSet),
-        fromJson
+        fromJson,
+        sort
     };
     
 };
