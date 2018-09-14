17bba3d26a9e4cce3ef35d8d2ad91e6f585782b0
Akshit Grover <akshit.grover2016@gmail.com>
2018-09-14 17:43:28 +0530

    Update jsdoc html

--- docs/scripts/linenumber.js
+++ docs/scripts/linenumber.js
@@ -1,6 +1,6 @@
 /*global document */
 (function() {
-    var source = document.getElementsByClassName("prettyprint source linenums");
+    var source = document.getElementsByClassName('prettyprint source linenums');
     var i = 0;
     var lineNumber = 0;
     var lineId;
@@ -10,15 +10,15 @@
 
     if (source && source[0]) {
         anchorHash = document.location.hash.substring(1);
-        lines = source[0].getElementsByTagName("li");
+        lines = source[0].getElementsByTagName('li');
         totalLines = lines.length;
 
         for (; i < totalLines; i++) {
             lineNumber++;
-            lineId = "line" + lineNumber;
+            lineId = 'line' + lineNumber;
             lines[i].id = lineId;
             if (lineId === anchorHash) {
-                lines[i].className += " selected";
+                lines[i].className += ' selected';
             }
         }
     }
